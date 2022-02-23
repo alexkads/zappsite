6f4a891710388eefbf6f23b9e9db3d6a11ab4247
alexkads <alexkads@gmail.com>
2022-02-23 13:34:28 -0300

    PDF Viewer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,24 @@
 import Link from "next/link";
+import PDFViewer from "pdf-viewer-reactjs";
 
 export default function Home() {
   return (
     <>
       <div>Meu Cartão de Visitas</div>
+      
+      <br />
+
+      <PDFViewer
+        document={{
+          url: "/downloads/MKT_Filippi_Arruda.pdf",
+        }}
+      />
+
+      <br />
+
       <Link href="/downloads/MKT_Filippi_Arruda.pdf">
         <a>Baixar</a>
-      </Link>
+      </Link> 
     </>
   );
 }
