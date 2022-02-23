e9c7935201802f6f912ae2af7b05fe573e877ae0
alexkads <alexkads@gmail.com>
2022-02-23 15:48:11 -0300

    Novo PDF-Reader

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,25 @@
 import Link from "next/link";
 
+import dynamic from "next/dynamic";
+
+const PDFViewer = dynamic(() => import("../components/pdf-viewer"), {
+  ssr: false
+});
+
 export default function Home() {
   return (
     <>
-      <div>Meu Cartão de Visitas</div>    
-      
+      <div>Meu Cartão de Visitas</div>     
+
+      <br />
+
+      <PDFViewer file="/downloads/MKT_Filippi_Arruda.pdf"/>
+
       <br />
 
       <Link href="/downloads/MKT_Filippi_Arruda.pdf">
         <a>Baixar</a>
-      </Link> 
+      </Link>
     </>
   );
 }
